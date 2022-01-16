d58dfbc6d98208fbedcea9a93230642e5338f77a
Abraones <abraaobenites@gmail.com>
2022-01-16 15:35:18 -0300

    Funcionando o filtro de ativos com Saldo

--- src/pages/bianca/index.jsx
+++ src/pages/bianca/index.jsx
@@ -2,37 +2,50 @@ export default function Trades({data}){
     //console.log(data)
     const ativos = data.balances;
     
-    const meusAtivos = ativos.map(()=>{
-        return ativos.free > 0.00000000 
-    })
-    //console.log(meusAtivos)
-    
+    const exAtivos = [
+        {asset: 'BTC', free: 0.00100000 ,locked: 0.00000000},
+        {asset: 'BUSD', free: 40.00000000 ,locked: 0.00000000},
+        {asset: 'ETH', free: 0.00400000 ,locked: 0.00000000},
+        {asset: 'LTC', free: 0.00000000 ,locked: 0.00000000}
+    ]
+    function ativosMaior(props){
+        var comSaldo = [];
+        props.map((props)=>{
+            if(props.free > 0){
+                comSaldo.push(props)
+                
+            }
+        })
+        return comSaldo
+    }
 
     return(
         <div>
             <h1>Aqui aparecerão meus ativos</h1>
-            {ativos.map((ativo) =>(
+
+            {ativosMaior(ativos).map((ativo) =>(
                 <p>{ativo.asset} : {ativo.free}</p>
             ))}
+
+           {/*  {exAtivos.map((ativo) =>(
+                <p>{ativo.asset} : {ativo.free}</p>
+            ))} */}
+            {/* {console.log(ativosMaior(exAtivos))} */}
             <h2></h2>
         </div>
     )
 }
 
-
-
 export const getStaticProps = async () => {
-
+    //Conecta com a Binance
     const { Spot } = require('@binance/connector');
     const apiKey = process.env.API_KEY;
     const apiSecret = process.env.SECRET_KEY;
     const client = new Spot(apiKey, apiSecret);
 
+    // Chama os dados do Metódo acconunt
     const response = await client.account();
     const data = await response.data
-    //console.log(response.data.balances)
-   
-    //const data = await res.json()
 
     return{
         props:{
